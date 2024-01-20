2ade5e8303e623240dd737bec9e6b469e097d7b9
Pratik Bodawala <pratikb@york.ie>
2024-01-20 09:56:14 +0530

    :sparkles: basic setup for nginx template

--- run.py
+++ run.py
@@ -1,13 +1,41 @@
 #!/usr/bin/env python
+import json
 from argparse import ArgumentParser
+from urllib.request import urlopen
+import logging
 
 parser = ArgumentParser()
 parser.add_argument("-u", "--url", dest="url", required=True,
                     help="url to fetch data", metavar="path")
-parser.add_argument("-q", "--quiet",
-                    action="store_false", dest="verbose", default=True,
-                    help="don't print status messages to stdout")
+parser.add_argument("-v", "--verbose",
+                    action="store_true", dest="verbose", default=False,
+                    help="print status messages to stdout")
 
 args = parser.parse_args()
 
-print(args)
+if args.verbose:
+    logging.basicConfig(level=logging.INFO)
+else:
+    logging.basicConfig(level=logging.ERROR)
+
+logging.info(f'fetching url: {args.url}')
+
+json_data = urlopen(args.url).read()
+logging.info(f'json_data: {json_data}')
+
+for item in json.loads(urlopen(args.url).read()):
+    logging.info(f'started processing {item["domain"]}')
+    with open(f"./nginx/conf.d/{item['domain']}.conf", "w") as f:
+        f.write(f"""
+server {{
+    listen 80;
+    server_name {item['domain']};
+    location / {{
+        proxy_pass {item['cf']};
+    }}
+    access_log /var/log/nginx/access-{item['domain']}.log;
+    error_log /var/log/nginx/error-{item['domain']}.log;
+}}
+""")
+    logging.info(f'finished processing {item["domain"]}')
+logging.info('done')

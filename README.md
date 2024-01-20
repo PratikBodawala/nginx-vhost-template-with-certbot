971fb00f5baaf404e91cca02b5be15c73b851114
Pratik Bodawala <pratikb@york.ie>
2024-01-20 11:36:33 +0530

    :sparkles: auto certbot with dry mode

--- README.md
+++ README.md
@@ -10,10 +10,10 @@
 ```bash
 ./run.py
 
-usage: run.py [-h] -u path [-v]
-run.py: error: the following arguments are required: -u/--url
+usage: run.py [-h] -u path -e email [--production] [-v]
+run.py: error: the following arguments are required: -u/--url, -e/--email
 ```
 
 ```bash
-./run.py -u https://268f-103-240-162-120.ngrok-free.app/sample_domains.json --verbose
+./run.py -u https://268f-103-240-162-120.ngrok-free.app/sample_domains.json --verbose -e pratikb@york.ie
 ```
--- run.py
+++ run.py
@@ -4,13 +4,18 @@ from argparse import ArgumentParser
 from urllib.request import urlopen
 import logging
 import glob
-import os
+from os import symlink, remove
+from os.path import exists
 from certbot.main import main as certbot_main
-
+from shutil import copy
 
 parser = ArgumentParser()
 parser.add_argument("-u", "--url", dest="url", required=True,
                     help="url to fetch data", metavar="path")
+parser.add_argument('-e', '--email', dest='email', required=True,
+                    help="email address to register with letsencrypt", metavar="email")
+parser.add_argument('--production', dest='production', action='store_true',
+                    help="use production letsencrypt server", default=False)
 parser.add_argument("-v", "--verbose",
                     action="store_true", dest="verbose", default=False,
                     help="print status messages to stdout")
@@ -23,18 +28,23 @@ else:
     logging.basicConfig(level=logging.ERROR)
 
 logging.info('starting cleanup')
-for f in glob.glob("./nginx/conf.d/*.conf"):
-    os.remove(f)
-logging.info('cleanup done')
 
+for f in glob.glob("./nginx/conf.d/*.conf"):
+    remove(f)
+
+logging.info('cleanup done')
 
 logging.info(f'fetching url: {args.url}')
 
 json_data = urlopen(args.url).read()
 logging.info(f'json_data: {json_data}')
 
+domains = []
+
 for item in json.loads(urlopen(args.url).read()):
     logging.info(f'started processing {item["domain"]}')
+    domains.append(item['domain'])
+
     with open(f"./nginx/conf.d/{item['domain']}.conf", "w") as f:
         f.write(f"""
 server {{
@@ -47,21 +57,37 @@ server {{
     error_log /var/log/nginx/error-{item['domain']}.log;
 }}
 """)
+
     logging.info(f'finished processing {item["domain"]}')
 logging.info('done')
 
-certbot_main([
+certbot_args = [
     '--nginx',
     '--agree-tos',
-    '--no-eff-email',
+    '-m', args.email,
     '--redirect',
-    '--register-unsafely-without-email',
-    '--staging',
     '--non-interactive',
     '--expand',
     '--force-renewal',
     '--work-dir', '/tmp/work',
     '--config-dir', '/tmp/config',
     '--logs-dir', '/tmp/logs',
+    '--domains', ','.join(domains)
 ]
-)
+
+for file in glob.glob("./nginx/conf.d/*.conf"):
+    copy(file, "/etc/nginx/sites-available/")
+    soft_dest = f"/etc/nginx/sites-enabled/{file.split('/')[-1]}"
+
+    if exists(soft_dest):
+        remove(soft_dest)
+
+    symlink(f"/etc/nginx/sites-available/{file.split('/')[-1]}", soft_dest)
+
+if not args.production:
+    certbot_args.extend(['--staging',
+                         '--dry-run',
+                         '--test-cert',
+                         'certonly'])
+
+certbot_main(certbot_args)
